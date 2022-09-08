29d2954abcc9b259ba37a1def124ba0d7dfe5bae
Mikhail Voronov <smirk06@yandex.ru>
2022-09-08 19:34:06 +0300

    init

---
+++ src/main/java/com/github/smkjke/insta/InstaApplication.java
@@ -0,0 +1,13 @@
+package com.github.smkjke.insta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/smkjke/insta/InstaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.smkjke.insta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
