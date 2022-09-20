d882612f4eab3669d96752c4d5d92d9154216539
Mikhail Voronov <smirk06@yandex.ru>
2022-09-20 23:51:03 +0300

    Created repositories

---
+++ src/main/java/com/github/smkjke/insta/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.github.smkjke.insta.repository;
+
+import com.github.smkjke.insta.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByUserName(String username);
+
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(Long id);
+}
